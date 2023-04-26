50ef68b4437207d7418d7c1b2fd539cbdd83145f
Dosemanai <dsemnai@gmail.com>
2023-04-26 22:55:14 +0900

    Ready for First Deployment Test

---
+++ app.js
@@ -0,0 +1,69 @@
+if (process.env.NODE_ENV !== 'Production') require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const setupRoutes = require('./routes.js');
+const rateLimiter = require('express-rate-limit');
+const { MongoClient } = require('mongodb');
+
+const appPort = process.env.PORT || 3001;
+const user = process.env.MONGODB_USER;
+const password = process.env.MONGODB_PASSWORD;
+const cluster = process.env.MONGODB_CLUSTER;
+const database = process.env.MONGODB_DATABASE;
+const mguri = `mongodb+srv://${user}:${password}@${cluster}/${database}`;
+
+async function main() {
+	try {
+		client = await MongoClient.connect(mguri, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+
+		console.log(`MongoDB Connection Established.`);
+		const db = client.db();
+
+		process.on('SIGINT', () => {
+			client.close();
+			console.log('MongoClient Connection Closed.');
+			process.exit(0);
+		});
+
+		setupAppUsage();
+		setupRoutes(app, db);
+		startServer(appPort);
+	} catch (err) {
+		console.error(`MongoDB Connection Failed.`);
+	}
+}
+
+function setupAppUsage() {
+	app.use(rateLimiter({ windowMs: 60 * 1000, max: 15 }));
+
+	//Serve .br files with Brotli compression
+	app.get('*.br', function (req, res, next) {
+		res.set('Content-Encoding', 'br');
+		if (req.url.endsWith('.js.br')) res.setHeader('Content-Type', 'application/javascript');
+		else if (req.url.endsWith('.wasm.br')) res.setHeader('Content-Type', 'application/wasm');
+		else if (req.url.endsWith('.data.br')) res.setHeader('Content-Type', 'application/octet-stream');
+		next();
+	});
+
+	//app.use(compressor); // this is slow for some reason. we'll look into it later.
+	app.use('/static/', express.static(path.join(__dirname, 'public')));
+	app.use(express.urlencoded({ extended: false }));
+	app.use(express.json());
+}
+
+function startServer() {
+	app.listen(appPort, err => {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		console.log(`Express server listening on port ${appPort}`);
+	});
+}
+
+main().catch(err);
---
+++ routes.js
@@ -0,0 +1,54 @@
+function setupRoutes(app, db) {
+	app.get('/', (req, res) => {
+		res.sendFile(path.join(__dirname, 'public', 'test.html'));
+	});
+
+	app.post('/game/test/pushbtn', (req, res) => {
+		console.log(`Press Button ${req.body.times}!`);
+		db.collection('test')
+			.updateOne({}, { $inc: { button_pressed: req.body.times } })
+			.then(doc => res.sendStatus(200))
+			.catch(err =>
+				res.status(500).json({
+					error: 'Internal server error: could not push data to button.',
+				})
+			);
+	});
+
+	// app.post('/unity/hello', (req, res, next) => {
+	// 	console.log('Hello JS!');
+	// 	res.sendStatus(200);
+	// });
+
+	// app.post('/unity/hello/world', (req, res, next) => {
+	// 	console.log(`Hello JS from ${req.body.u8message}`);
+	// 	res.sendStatus(200);
+	// });
+
+	// app.get('/unity/pullbtn', (req, res, next) => {
+	// 	db.collection('test')
+	// 		.findOne({})
+	// 		.then(doc => {
+	// 			res.send(doc.button_pressed.toString());
+	// 		})
+	// 		.catch(err =>
+	// 			res.status(500).json({
+	// 				error: 'Internal server error: could not pull data from button.',
+	// 			})
+	// 		);
+	// });
+
+	// app.post('/unity/pushbtn', (req, res, next) => {
+	// 	console.log(`Press Button ${req.body.times}!`);
+	// 	db.collection('test')
+	// 		.updateOne({}, { $inc: { button_pressed: req.body.times } })
+	// 		.then(doc => res.sendStatus(200))
+	// 		.catch(err =>
+	// 			res.status(500).json({
+	// 				error: 'Internal server error: could not push data to button.',
+	// 			})
+	// 		);
+	// });
+}
+
+module.exports = setupRoutes;
